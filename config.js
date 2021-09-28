f7e8a6f0fcf97304e7ef962acc0a83c697f98eb8
sakurakoding <91261477+sakurakoding@users.noreply.github.com>
2021-09-28 21:39:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ global.APIKeys = { // APIKey nya disini
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://pencarikode.xyz': 'pais',
-  'https://api.xteam.xyz': 'apikeymu',
+  'https://api.xteam.xyz': '8d78801c391ee421',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'apivinz',
 }
