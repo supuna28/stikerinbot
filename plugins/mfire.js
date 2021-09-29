31b1df91f6e9cc8579705d8252d6a467719f3cf6
sakurakoding <91261477+sakurakoding@users.noreply.github.com>
2021-09-29 14:30:20 +0530

    Create mfire.js

---
+++ plugins/mfire.js
@@ -0,0 +1,36 @@
+
+
+const mediafireDl = async (url) => {
+
+const res = await axios.get(url) 
+
+const $ = cheerio.load(res.data)
+
+const hasil = []
+
+const link = $('a#downloadButton').attr('href')
+
+const size = $('a#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '')
+
+const seplit = link.split('/')
+
+const nama = seplit[5]
+
+mime = nama.split('.')
+
+mime = mime[1]
+
+hasil.push({ nama, mime, size, link })
+
+return hasil
+
+}
+handler.help = ['mfire'].map(v => v + ' <pencarian>')
+
+handler.tags = ['downloader']
+
+handler.command = /^(meadiafirei).map(v => v + ' <url>')
+
+handler.exp = 0
+
+module.exports = { mediafireDl }
